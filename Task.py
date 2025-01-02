8a2b35b6f6c09bb4f83e7d600d4a074c95836f51
luc6987 <148687846+luc6987@users.noreply.github.com>
2025-01-02 22:37:11 +0100

    change

--- Task.py
+++ Task.py
@@ -113,45 +113,7 @@ def minimal_enclosing_sphere(points):
     random.shuffle(points)
     return welzl(points, [])
 
-# Test cases
-def test_task1():
-    """Test cases for minimal enclosing sphere."""
-    # Test 1: Single point
-    points = [(0, 0, 0)]
-    sphere = minimal_enclosing_sphere(points)
-    assert np.allclose(sphere.center, [0, 0, 0])
-    assert np.isclose(sphere.radius, 0)
-    print("Test 1 passed!")
 
-    # Test 2: Two points
-    points = [(0, 0, 0), (2, 0, 0)]
-    sphere = minimal_enclosing_sphere(points)
-    assert np.allclose(sphere.center, [1, 0, 0])
-    assert np.isclose(sphere.radius, 1)
-    print("Test 2 passed!")
-
-    # Test 3: Three points
-    points = [(-10, 0, 0), (10, 0, 0), (0, 1, 0)]
-    sphere = minimal_enclosing_sphere(points)
-    assert np.allclose(sphere.center, [0, 0, 0])
-    assert np.isclose(sphere.radius, 10)
-    print("Test 3 passed!")
-
-    # Test 4: Four points
-    points = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
-    sphere = minimal_enclosing_sphere(points)
-    assert np.allclose(sphere.center, [0, 0, 0])
-    assert np.isclose(sphere.radius, np.sqrt(26))
-    print("Test 4 passed!")
-
- 
-
-    print("All test cases passed!")
-
-
-
-print("---------Question 1------------")
-test_task1()
 
 
 ## Question 2:
@@ -164,72 +126,6 @@ def task2(points,emu):
     filtration = minimal_enclosing_sphere(points_chosen).radius
 
     return filtration
-
-# Test cases
-def test_task2():
-        P = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
-
-        enu=[0]
-        assert np.allclose(task2(P,enu), 0)  
-        print(f"Test({enu})passed!")
-
-        enu=[1]
-        assert np.allclose(task2(P,enu), 0)  
-        print(f"Test({enu})passed!")
-
-        enu=[2]
-        assert np.allclose(task2(P,enu), 0)  
-        print(f"Test({enu})passed!")
-
-        enu=[3]
-        assert np.allclose(task2(P,enu), 0)
-        print(f"Test({enu})passed!")
-
-        enu=[2,1]
-        assert np.allclose(task2(P,enu), 3.53553)   
-        print(f"Test({enu})passed!")
-
-        enu=[1,0]
-        assert np.allclose(task2(P,enu), 3.67425)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,2]
-        assert np.allclose(task2(P,enu), 4)   
-        print(f"Test({enu})passed!")
-
-        enu=[2,0]
-        assert np.allclose(task2(P,enu), 4.12311)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,0]
-        assert np.allclose(task2(P,enu), 4.12311)   
-        print(f"Test({enu})passed!")
-
-        enu=[2,1,0]
-        assert np.allclose(task2(P,enu), 4.39525)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,2,0]
-        assert np.allclose(task2(P,enu), 4.71495)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,1]
-        assert np.allclose(task2(P,enu), 4.94975)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,2,1]
-        assert np.allclose(task2(P,enu), 5)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,1,0]
-        assert np.allclose(task2(P,enu), 5.04975)   
-        print(f"Test({enu})passed!")
-
-        enu=[3,2,1,0]
-        assert np.allclose(task2(P,enu), 5.09902)   
-        print(f"Test({enu})passed!")
-
-        print("Test 2 all passed! ")
 
 def enum_simplex2(points):
     "énumère et affiche les simplexes avec la valeur de filtrage"
@@ -254,10 +150,9 @@ def enum_simplex2(points):
         print(f"({enum}) -> {filtration}")
 
 
-print("---------Question 2------------")
-test_task2()
 
-print("---------Question 3------------")
+
+
 
 from itertools import combinations #On utilise ici une bibliothèque pour le travail combinatoire -> à faire à la main plus tard.
 
@@ -308,11 +203,7 @@ def task3(points,l):
     for key, value in simplex.items():
         print(f"{key} -> {value.radius}")
 
-def test_task3():
-    P = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
-    task3(P,1000)
 
-test_task3()
 
 def task4(points):
     """"Reuse the LP-type algorithm with new parameters in order to determine
---
+++ test.py
@@ -0,0 +1,116 @@
+import Task as t
+import numpy as np
+
+# Test cases
+def test_task1():
+    """Test cases for minimal enclosing sphere."""
+    # Test 1: Single point
+    points = [(0, 0, 0)]
+    sphere = t.minimal_enclosing_sphere(points)
+    assert np.allclose(sphere.center, [0, 0, 0])
+    assert np.isclose(sphere.radius, 0)
+    print("Test 1 passed!")
+
+    # Test 2: Two points
+    points = [(0, 0, 0), (2, 0, 0)]
+    sphere = t.minimal_enclosing_sphere(points)
+    assert np.allclose(sphere.center, [1, 0, 0])
+    assert np.isclose(sphere.radius, 1)
+    print("Test 2 passed!")
+
+    # Test 3: Three points
+    points = [(-10, 0, 0), (10, 0, 0), (0, 1, 0)]
+    sphere = t.minimal_enclosing_sphere(points)
+    assert np.allclose(sphere.center, [0, 0, 0])
+    assert np.isclose(sphere.radius, 10)
+    print("Test 3 passed!")
+
+    # Test 4: Four points
+    points = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
+    sphere = t.minimal_enclosing_sphere(points)
+    assert np.allclose(sphere.center, [0, 0, 0])
+    assert np.isclose(sphere.radius, np.sqrt(26))
+    print("Test 4 passed!")
+
+ 
+
+    print("All test cases passed!")
+
+
+# Test cases
+def test_task2():
+        P = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
+
+        enu=[0]
+        assert np.allclose(t.task2(P,enu), 0)  
+        print(f"Test({enu})passed!")
+
+        enu=[1]
+        assert np.allclose(t.task2(P,enu), 0)  
+        print(f"Test({enu})passed!")
+
+        enu=[2]
+        assert np.allclose(t.task2(P,enu), 0)  
+        print(f"Test({enu})passed!")
+
+        enu=[3]
+        assert np.allclose(t.task2(P,enu), 0)
+        print(f"Test({enu})passed!")
+
+        enu=[2,1]
+        assert np.allclose(t.task2(P,enu), 3.53553)   
+        print(f"Test({enu})passed!")
+
+        enu=[1,0]
+        assert np.allclose(t.task2(P,enu), 3.67425)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,2]
+        assert np.allclose(t.task2(P,enu), 4)   
+        print(f"Test({enu})passed!")
+
+        enu=[2,0]
+        assert np.allclose(t.task2(P,enu), 4.12311)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,0]
+        assert np.allclose(t.task2(P,enu), 4.12311)   
+        print(f"Test({enu})passed!")
+
+        enu=[2,1,0]
+        assert np.allclose(t.task2(P,enu), 4.39525)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,2,0]
+        assert np.allclose(t.task2(P,enu), 4.71495)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,1]
+        assert np.allclose(t.task2(P,enu), 4.94975)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,2,1]
+        assert np.allclose(t.task2(P,enu), 5)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,1,0]
+        assert np.allclose(t.task2(P,enu), 5.04975)   
+        print(f"Test({enu})passed!")
+
+        enu=[3,2,1,0]
+        assert np.allclose(t.task2(P,enu), 5.09902)   
+        print(f"Test({enu})passed!")
+
+        print("Test 2 all passed! ")
+def test_task3():
+    P = [(5, 0, 1), (-1, -3, 4), (-1, -4, -3), (-1, 4, -3)]
+    t.task3(P,1000)
+
+
+print("---------Question 1------------")
+test_task1()
+print("---------Question 2------------")
+test_task2()
+print("---------Question 3------------")
+test_task3()
+
